8f9558191a42839a6fe5c806e7b869903110ed65
Aayush Thakur <ashcold21@gmail.com>
2024-06-03 14:10:05 +0530

    Implement Left Sidebar functionality

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,26 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Aayush', lastName: 'JSM' };
+
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image src='/icons/logo.svg' height={30} width={30} alt='logo' />
+          <div className="">
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
